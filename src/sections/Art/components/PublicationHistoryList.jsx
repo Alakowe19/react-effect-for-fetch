e5cef64171bd41ff5a4e4253c9189a1093d94811
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-05-08 10:57:41 +0100

    Image successfully added

--- src/sections/Art/components/PublicationHistoryList.jsx
+++ src/sections/Art/components/PublicationHistoryList.jsx
@@ -1,4 +1,4 @@
-const PublicationHistoryList = ({ children, name }) => { //did the second way to render my props here
+const PublicationHistoryList = ({ children, name, }) => { //did the second way to render my props here
   return (
     <div>
       <h4>{name}</h4>
@@ -6,8 +6,8 @@ const PublicationHistoryList = ({ children, name }) => { //did the second way to
         {children}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   return (
@@ -17,9 +17,15 @@ const App = () => {
         <li>Léon Mancino, “La descente de la courtille,” L’art 9 (Apr. 1877), p. 70.</li>
         <li>Anonymous [possibly Gaston Vassy], “La journée à Paris: L’exposition des impressionnistes,” L’événement, Apr. 6, 1877, p. 2.</li>
         <li>Learning react</li>
+        <br></br> <br></br>
+        <img src="https://images.pexels.com/photos/16499261/pexels-photo-16499261/free-photo-of-boy-in-denim-jacket-at-night.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="Image is from pexels.com"
+
+        />
       </PublicationHistoryList>
+
     </div>
-  );
-};
+  )
+}
 
 export default App;
