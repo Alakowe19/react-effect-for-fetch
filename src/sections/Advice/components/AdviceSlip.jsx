6f8641fd8c2471533b387a10a24bf28731a0635f
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-05-09 16:34:31 +0100

    Truing to sort the art section

--- src/sections/Advice/components/AdviceSlip.jsx
+++ src/sections/Advice/components/AdviceSlip.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import FavouriteSlipsList from "./FavouriteSlipsList"
 
 const AdviceSlip = ()  =>  {
--- src/sections/Art/components/ArtList.jsx
+++ src/sections/Art/components/ArtList.jsx
@@ -2,6 +2,23 @@ import ArtListItem from "./ArtListItem"
 import PublicationHistoryList from "./PublicationHistoryList"
 
 const ArtList = () => {
+    const [arts, setArt] = useState([])
+
+    useEffect( () => {
+        const timer = setTimeout(() =>  {
+            const FetchArt = [
+
+                imgSrc: '',
+                name: 'Abiodun, Oluwagbemi',
+                artist: '',
+            ]
+            setArt(fetchArt)
+        }, 1000)
+
+        return () => clearTimeout (timer)
+    }, [])
+
+
 return (
 
     <div className="scroll-container">
@@ -10,6 +27,18 @@ return (
 
             <li>
 
+                {arts.map((art, index) =>
+                <ArtListItem 
+                    key={index}
+                    name={art.name}
+                    imageSrc={art.imageSrc}
+                    artist={art.artist}
+                />
+            )
+
+
+                }
+
             < ArtListItem 
             imageSrc = 'https://boolean-api-server.fly.dev/images/paris-street-rainy-day.jpg'
             name = "Paris Street;  Rainy Day"
@@ -17,10 +46,6 @@ return (
             />
 
             < PublicationHistoryList />
-            
-            
-               
-    
             
             </li>
 
